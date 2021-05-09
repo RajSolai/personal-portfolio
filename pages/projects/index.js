c3bb13977e660f838d679940feea9c1fb5f061f8
RajSolai <msraj085@gmail.com>
2021-05-09 14:17:44 +0530

    resolved the switch algorithm

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -10,10 +10,14 @@ import axios from "axios";
 
 export default function project() {
   const [theprojects, settheprojects] = useState([]);
+  const [unaltered, setUnaltered] = useState([]);
+  const [isLoading, setLoading] = useState(true);
   const [theprevtarget, setprevtarget] = useState(null);
   useEffect(() => {
     axios.get("https://projects-api-servlet.herokuapp.com/projects").then(res=>{
       settheprojects(res.data);
+      setUnaltered(res.data);
+      setLoading(false);
     });
   }, []);
   const changeCategory = (event) => {
@@ -23,12 +27,12 @@ export default function project() {
     setprevtarget(event.target);
     event.target.className += " category-btn-active";
     if (event.target.id) {
-      const filteredData = theprojects.filter(
+      const filteredData = unaltered.filter(
         (_data) => _data.searchkey == event.target.id
       );
       settheprojects(filteredData);
     } else {
-      settheprojects(theprojects);
+      settheprojects(unaltered);
     }
   };
   return (
@@ -82,21 +86,28 @@ export default function project() {
           </Fade>
         </div>
         <div className="project-holder">
-          {theprojects.length == 0 ? (
-            <p>Oops, Nothing Found here !</p>
-          ) : (
-            theprojects.map((_data, key) => (
-              <Fade bottom delay={100 + 90 * key}>
-                <ProjectCard
-                  key={key}
-                  name={_data.title}
-                  desc={_data.desc}
-                  link={_data.url}
-                  lang={_data.langs}
-                />
-              </Fade>
-            ))
-          )}
+          {
+            isLoading ? (
+              <p>Loading ...</p>
+            ) : (
+              theprojects.length == 0 ? (
+                <p>Oops, Nothing Found here !</p>
+              ) : (
+                theprojects.map((_data, key) => (
+                  <Fade bottom delay={100 + 90 * key}>
+                    <ProjectCard
+                      key={key}
+                      name={_data.title}
+                      desc={_data.desc}
+                      link={_data.url}
+                      lang={_data.langs}
+                    />
+                  </Fade>
+                ))
+              )    
+            )
+          }
+          
         </div>
       </div>
     </>
