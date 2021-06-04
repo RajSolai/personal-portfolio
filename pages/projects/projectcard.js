8ebc9d14da392119f4b42940b9fa8d4f29c7a1fd
Solai Raj <msraj085@gmail.com>
2021-06-04 13:56:42 +0530

    mobile design completed

--- pages/projects/projectcard.js
+++ pages/projects/projectcard.js
@@ -1,14 +1,6 @@
 import { FaGithub } from "react-icons/fa";
 
 export default function ProjectCard(props) {
-  const getData = () => {
-    let temp = [];
-    Axios.get("./api/projects").then((res) => {
-      temp = res.data;
-    });
-    return temp;
-  };
-
   return (
     <div className="project-card">
       <h3>{props.name}</h3>
