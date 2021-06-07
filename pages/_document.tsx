2d8ba675a8e71263797487fa142b681e86ea1529
Solai Raj <msraj085@gmail.com>
2021-06-07 20:02:48 +0530

    seo optimizations

---
+++ pages/_document.tsx
@@ -0,0 +1,49 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;800&display=swap"
+            rel="stylesheet"
+          ></link>
+          <meta
+            name="description"
+            content="This is meta description Sample. We can add up to 158."
+          ></meta>
+          <link rel="canonical" href="http://solairaj.rf.gd/" />
+          <meta name="robots" content="index, follow" />
+          <meta property="og:type" content="profile" />
+          <meta property="og:title" content="RajSolai - Developer" />
+          <meta
+            property="og:description"
+            content="Solai Raj's Profile. A Student , Web ,Mobile Developer IoT and AI enthusiast
+                IoT, Linux Guy"
+          />
+          <meta property="og:image" content="LINK TO THE IMAGE FILE" />
+          <meta property="og:url" content="http://solairaj.rf.gd/" />
+          <meta property="og:site_name" content="RajSolai" />
+          <meta name="twitter:title" content="RajSolai - Developer" />
+          <meta
+            name="twitter:description"
+            content="Student , Web ,Mobile Developer IoT and AI enthusiast
+            IoT, Linux Guy"
+          />
+          {/* TODO: add iamge for twitter card */}
+          <meta name="twitter:image" content="LINK TO IMAGE" />
+          <meta name="twitter:site" content="@RajSolai2" />
+          <meta name="twitter:creator" content="@RajSolai2" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -59,12 +59,7 @@ const Home: React.FC<any> = () => {
   return (
     <>
       <Head>
-        <title>Solai Raj</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;800&display=swap"
-          rel="stylesheet"
-        ></link>
+        <title>RajSolai - Developer</title>
       </Head>
       <nav>
         <ul>
